64f5e6f86fd61e21be2d3d70bc03f5cabd394b18
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-04-18 16:16:06 +0300

    Add: course-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-11.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution: #Kahn's algorithm
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        courses = { i:[] for i in range(numCourses) }
+        prereqs = { i:[] for i in range(numCourses) }
+        for crs, pre in prerequisites:
+            courses[crs].append(pre)
+            prereqs[pre].append(crs)
+
+        indegrees = { c: len(p) for c, p in courses.items() }
+        queue = deque({ c for c, p in courses.items() if p == [] })
+        res = []
+        while queue:
+            u = queue.popleft()
+            res.append(u)
+
+            for v in prereqs[u]:
+                indegrees[v] -= 1
+                if indegrees[v] == 0:
+                    queue.append(v)
+        
+        return res if len(res) == numCourses else []
