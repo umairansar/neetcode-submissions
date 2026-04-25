b75275183450bac2f783da12f766a8642cb0016d
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-04-25 21:49:49 +0300

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    '''
+    O(n * a) time and space
+    n = number of coins, a = amount, m = min coin
+    Lesson from bad solution 1, fixed here.
+    Force user to pick combinitions not permutations
+    '''
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        
+        memo = {}
+        def dfs(i, total):
+            if i == n or total > amount:
+                return 0
+            if total == amount:
+                return 1
+            if (i, total) in memo:
+                return memo[(i, total)]
+
+            memo[(i, total)] = dfs(i, total + coins[i]) + dfs(i+1, total)
+            return memo[(i, total)]
+        
+        return dfs(0, 0)
