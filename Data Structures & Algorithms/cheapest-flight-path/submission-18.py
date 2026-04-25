a3288d92fbe5476688f5f3108e84e485ed3c6766
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-04-25 17:23:46 +0300

    Add: cheapest-flight-path - submission-18

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-18.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        distances = [0 if i == src else float('inf') for i in range(n)]
+        jumps = [float('inf') for i in range(n)]
+        edges = {}
+        for s, d, dist in flights:
+            edges[s] = edges.get(s, []) + [(d, dist)]
+        print("e", edges)
+        heap = [(-1, 0, src)]
+        while heap:
+            # print("h", heap)
+            k_, dist, u = heapq.heappop(heap)
+            # print(k_, dist, u)
+            # if dist > distances[u]:
+            #     continue
+            neighbours = edges.get(u, [])
+            # print("ne", neighbours, jumps, distances)
+            for v, dist2 in neighbours:
+                newDist = dist + dist2
+                newK = k_ + 1
+                # print(newK, jumps[v], newDist, distances[v])
+                if newK <= k and newDist <= distances[v]:
+                    distances[v] = newDist
+                    jumps[v] = newK
+                    heapq.heappush(heap, (newK, newDist, v))
+        return -1 if distances[dst] == float('inf') else distances[dst]
