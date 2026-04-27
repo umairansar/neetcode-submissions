f94f2e7e6eaec715d003316dc809bf1f947c8f71
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-04-27 21:03:56 +0300

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        memo = {}
+        def f(i, s): # form decision tree of depth O(2^n) if n levels w/o memo
+            if i == n:
+                return 1 if s == target else 0
+            if (i, s) in memo:
+                return memo[(i, s)]
+            if target - s > suffix[i] or target - s < - suffix[i]:
+                return 0
+
+            memo[(i, s)] = f(i + 1, s + nums[i]) + f(i + 1, s - nums[i])
+            return memo[(i, s)]
+
+        n = len(nums)
+        suffix = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + nums[i]
+        return f(0, 0)
