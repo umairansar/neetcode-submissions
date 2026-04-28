f71940e4f85eac1b640c8dce1a90c5a74bced91e
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-04-29 00:03:43 +0300

    Add: continuous-subarray-sum - submission-7

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        prefix = [0] * (n+1)
+        for i in range(1, n+1):
+            prefix[i] = prefix[i-1] + nums[i-1] # prefix includes current index value
+        
+        map = {0:0}
+        for i in range(1, n+1):
+            if prefix[i] % k in map:
+                if abs(map[prefix[i] % k] - i) > 1:
+                    return True
+            else:
+                map[prefix[i] % k] = i
+        return False
