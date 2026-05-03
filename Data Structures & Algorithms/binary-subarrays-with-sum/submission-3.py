3ee103bb44aa71bd782a6a60c3e80043278d56c6
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-05-03 23:02:15 +0300

    Add: binary-subarrays-with-sum - submission-3

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        n = len(nums)
+        prefix = [0] * (n+1)
+        for i in range(1, n+1):
+            prefix[i] = prefix[i-1] + nums[i-1]
+        map = {0:1}
+        r = 0
+        res = 0
+        while r < n:
+            if prefix[r+1] - goal in map:
+                res += map[prefix[r+1] - goal]
+            map[prefix[r+1]] = 1 + map.get(prefix[r+1], 0)
+            r += 1
+        return res
