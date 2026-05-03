b0b3ee677c867f0eb5fb7b8d8c06d0d9c7fd7a57
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-05-04 00:29:17 +0300

    Add: subarray-product-less-than-k - submission-14

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        '''
+        Improved sliding window w/o using explicit O(n) space for prefixes
+        '''
+        n = len(nums)
+        curr = 1
+        res = 0
+        l, r = 0, 0
+        while r < n:
+            curr *= nums[r]
+            while curr >= k and l <= r:
+                curr //= nums[l]
+                l += 1 
+            res += r - l + 1
+            r += 1
+        return res
+        
