8173cafe58074adeaf68e9485ed2bfd5e3c995df
Muhammad Umair Ansar <41548595+umairansar@users.noreply.github.com>
2026-05-18 12:07:03 +0300

    Add: online-stock-span - submission-8

---
+++ Data Structures & Algorithms/online-stock-span/submission-8.py
@@ -0,0 +1,27 @@
+class StockSpanner: #mono decreasing
+
+    def __init__(self):
+        self.prices = []
+        self.stack = []
+        self.n = 0
+
+    def next(self, price: int) -> int:
+        self.prices.append(price)
+        self.n += 1
+        while self.stack and price > self.stack[-1][0]:
+            self.stack.pop()
+        self.stack.append((price, self.n-1))
+        print(self.stack)
+        if len(self.stack) == 1:
+            return self.stack[-1][1] + 1
+        prev = -1
+        for i in range(len(self.stack) - 2, -1, -1):
+            if self.stack[i][0] > self.stack[-1][0]:
+                prev = self.stack[i][1]
+                break
+        return self.stack[-1][1] - prev
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
